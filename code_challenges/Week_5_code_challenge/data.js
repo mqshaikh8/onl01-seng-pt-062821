872f834650b5e776e8ba41095b9652db1f114502
Mohammed Shaikh <46161085+mqshaikh8@users.noreply.github.com>
2021-07-28 15:33:27 -0400

    solution code challenge 5

---
+++ code_challenges/Week_5_code_challenge/data.js
@@ -0,0 +1,53 @@
+const data = 
+[
+    {
+      "anime": "Boruto: Naruto Next Generations",
+      "character": "Naruto Uzumaki",
+      "quote": "The many lives lost during long years of conflict... because of those selfless sacrifices, we are able to bathe in peace and prosperity now. To ingrain this history within the new generation will be a vital cog in helping to maintain the peace."
+    },
+    {
+      "anime": "Naruto Shippūden",
+      "character": "Naruto Uzumaki",
+      "quote": "There is no telling what kind of pain will come  after me, but... if I stop believing because of that, if the hero should change, it'll turn into another story from the one my master left behind. Then it wont be Naruto! I can't write novels like my master did. That's why the sequel has to come from the life I live no matter how great the pain, I'll continue walking because that's who Naruto is!"
+    },
+    {
+      "anime": "Naruto",
+      "character": "Naruto Uzumaki",
+      "quote": "Because they saved me from myself, they rescued me from my loneliness. They were the first to accept me for who I am. They're my friends."
+    },
+    {
+      "anime": "Naruto",
+      "character": "Naruto Uzumaki",
+      "quote": "Once you question your own belief, it's over."
+    },
+    {
+      "anime": "Naruto",
+      "character": "Naruto Uzumaki",
+      "quote": "Don't underestimate me! I don't quit and I don't run! You can act tough all you want! You're not gonna scare me off! No way! I don't care if I DO get stuck as a Genin for the rest of my life! I'll still be Hokage someday!"
+    },
+    {
+      "anime": "Naruto",
+      "character": "Naruto Uzumaki",
+      "quote": "People who continue to put their lives on the line to defend their faith become heroes and continue to exist on in legend."
+    },
+    {
+      "anime": "Naruto",
+      "character": "Naruto Uzumaki",
+      "quote": "When you give up, your dreams and everything else, they're gone."
+    },
+    {
+      "anime": "Naruto",
+      "character": "Naruto Uzumaki",
+      "quote": "Hard work is worthless for those that don't believe in themselves."
+    },
+    {
+      "anime": "Naruto",
+      "character": "Naruto Uzumaki",
+      "quote": "If you don't like the hand that fate's dealt you with, fight for a new one!"
+    },
+    {
+      "anime": "Naruto",
+      "character": "Naruto Uzumaki",
+      "quote": "The pain of being alone is completely out of this world, isn't it? I don't know why, but I understand your feelings so much, it actually hurts."
+    }
+  ]
---
+++ code_challenges/Week_5_code_challenge/index.md
@@ -0,0 +1,24 @@
+your quote will look like this:
+
+
+
+<div class="quotes">
+    <div class="quote-container">
+        <p>
+        The many lives lost during long years of conflict... because of those selfless sacrifices, we are able to bathe in peace and prosperity now. To ingrain this history within the new generation will be a vital cog in helping to maintain the peace.
+        </p>
+        <p>Naruto Uzumaki(Boruto: Naruto Next Generations)</p>
+        <button class="del-button">Delete</button>
+    </div>
+    <!-- other quotes -->
+</div>
+
+
+- Be careful about class/ id names. The css will not kick in otherwise
+
+Tasks:
+1) Display all quotes in the `quotes` div
+2) Build out the delete button
+3) Form should be able to add quotes to list on the dom and the array `data`
+4) Bonus: The `random quote` button should display a random quote in the pTag. If the quote is liked, it should add to the dom and the `data` array
+5) **Bonus**: Make sure the form only submits if all fields are field. Otherwise it displays an error asking the user to fill out all fields
