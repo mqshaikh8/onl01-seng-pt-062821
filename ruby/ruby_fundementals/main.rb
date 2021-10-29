c1a7dfcf5e1263a599902ee88f5014273af7984f
Mohammed Shaikh <46161085+mqshaikh8@users.noreply.github.com>
2021-10-29 10:04:41 -0400

    ph3 week 1 ruby fundementals

---
+++ ruby/ruby_fundementals/main.rb
@@ -0,0 +1,77 @@
+require 'colorize'
+
+
+# test = 'test'
+
+# # puts test
+
+
+def greet
+    'Hi'
+end
+
+
+def greeting(name = 'Ruby Programmer')
+    "#{greet} #{name}"
+end
+
+puts greeting('Liz')
+puts greeting('Joe')
+puts greeting()
+
+
+x = 1
+
+# if x >2
+#     puts 'X is more than 2'
+# elsif x < 2 
+#     puts 'X is less than 2'
+# elsif x == 2
+#     puts 'X is equal to 2'
+# else
+#     puts 'whatever'
+# end
+
+# unless x > 2
+#     puts 'X is more than 2'
+# else
+#     puts 'X is not more than 2'
+# end
+
+
+# arr = [1,2,3,4,5,6,7,8,9]
+
+# # puts arr.length
+
+# puts arr.size
+
+# arr.find((argument) => code)
+
+# puts arr.count{|x| x.even? }
+
+
+# collection.each do |variable|
+#     code
+# end
+
+# puts arr.each{|i| i + 1}
+
+# arr.each do |i|
+#     puts i + 1
+# end
+
+# grades = {
+#     "Jane Doe" =>  10,
+#     "John Doe" => 20,
+#     Mike: 30
+# }
+
+# grades["Jane Doe"]
+
+# grades[:Mike]
+
+
+
+
+
+
